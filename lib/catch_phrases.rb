198d2d7102387007c3e442479b1e09fe12b5f888
Erik Huang <erikhuangdrums@gmail.com>
2020-12-16 21:43:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,20 +1,17 @@
 $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
-  phrase = "It's-a me, Mario!"
-  puts phrase
+
 end
 
 def toadstool
-  puts $status
+
 end
 
 def link
-  puts "It's Dangerous To Go Alone! Take This."
+  
 end
 
 def all_phrases
-  mario
-  toadstool
-  link
+
 end
